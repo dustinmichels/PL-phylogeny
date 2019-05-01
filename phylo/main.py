cf451d49ef2716b719f8804ead6aa1b620cf9e12
Dustin Michels <dustin7538@gmail.com>
2019-04-30 22:35:08 -0600

    rename snippets folder

--- phylo/main.py
+++ phylo/main.py
@@ -1,11 +1,12 @@
 import os
 import json
+from IPython.display import display, Markdown, Latex
 
 
 def load_snippets_data(name="mult"):
     package_directory = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(
-        package_directory, "..", "snippets", name, "out", "data.json"
+        package_directory, "..", "code_snippets", name, "out", "data.json"
     )
     with open(data_path) as f:
         data = json.load(f)
@@ -15,9 +16,19 @@ def load_snippets_data(name="mult"):
 def load_snippets_md(name="mult"):
     package_directory = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(
-        package_directory, "..", "snippets", name, "out", "snippets.md"
+        package_directory, "..", "code_snippets", name, "out", "snippets.md"
     )
     with open(data_path) as f:
         md = f.read()
     return md
 
+
+def code_to_markdown(lang1: str, lang2: str, data_dict: dict, display=False) -> str:
+    """Given dict with <lang: code> data, create markdown string repr"""
+    md = ""
+    for lang in [lang1, lang2]:
+        code = data_dict[lang]
+        md += f"**{lang}**\n\n```{lang}\n{code}\n```\n\n----\n\n"
+    if display:
+        display(Markdown(md))
+    return md
